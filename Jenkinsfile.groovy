528917cafb7a3b96ea548eb1fea48812143e23c3
marize98 <35236673+marize98@users.noreply.github.com>
2023-07-10 11:20:40 -0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,38 +1,41 @@
 pipeline {
-	    agent any
-		stages {
-			stage('Build') {
-			    steps {
-			        echo 'Building'
-			    }
-			}
-			stage('Test') {
-			    steps {
-			        echo 'Testing'
-			    }
-			}
-			stage('Deploy') {
-			    steps {
-			        echo 'Deploying'
-			    }
-			}
-		}
-	    post {
-	        always {
-	            echo 'This will always run'
-	        }
-	        success {
-	            echo 'This will run only if successful'
-	        }
-	        failure {
-	            echo 'This will run only if failed'
-	        }
-	        unstable {
-	            echo 'This will run only if the run was marked as unstable'
-	        }
-	        changed {
-	            echo 'This will run only if the state of the Pipeline has changed'
-	            echo 'For example, if the Pipeline was previously failing but is now successful'
-	        }
-	    }
-	}
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout scm
+            }
+        }
+        stage('Build') {
+            steps {
+                sh './gradlew build'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh './gradlew test'
+            }
+        }
+        stage('Deploy') {
+            when {
+                branch 'master'
+            }
+            steps {
+                sh './deploy.sh'
+            }
+        }
+    }
+
+    post {
+        always {
+            junit 'build/test-results/**/*.xml'
+        }
+        success {
+            slackSend channel: '#builds', color: 'good', message: 'Build successful!'
+        }
+        failure {
+            slackSend channel: '#builds', color: 'danger', message: 'Build failed!'
+        }
+    }
+}
